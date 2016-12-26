80ad0aaf78d18458dcf7320a71cf030337dec5aa
rgbosque <rgbosquepython@gmail.com>
2016-12-26 17:08:49 +0800

    Modified templates and add comment form.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_date', 'published_date')
 
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'text', 'approved_comment')
+
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
